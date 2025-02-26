d3b921b8f8b92b5d899164ba775a004d4f8b3bd5
Elena M <elenam@morris.umn.edu>
2025-02-26 12:43:54 -0600

    starting code for BST with empty node

---
+++ src/bst/BSTCount.java
@@ -0,0 +1,70 @@
+package bst;
+
+public class BSTCount {
+	private BSTNode root;
+	
+	/* 
+	 * Creates an empty tree that consists just of a single empty node
+	 * 
+	 */
+	public BSTCount() {
+		// Fill in the code according to the description 
+	}
+	
+	/*
+	 * The method takes a word and updates the tree accordingly:
+	 * If the word already occurs in the tree, its count gets incremented.
+	 * If it doesn't occur in the tree, it is added according to the 
+	 * binary search order.
+	 */
+	public void add(String word) {
+		// If the tree is still empty:
+		if (root == EmptyBSTNode.UNIQUE_INSTANCE) {
+			// Fill in the code according to the description
+		} else {
+			BSTNode current = root;
+			// The loop finds the position for the word.
+			// It stops when current points to the node containing the 
+			// word. 
+			// If the word is not found, it is added to the tree and 
+			// the method returns
+			while (!word.equals(current.getWord())){
+				// Fill in the code according to the description above
+			}
+			// Fill in the code according to the description
+		}
+	}
+	
+	/*
+	 * The method starts recursion on the root of the tree to 
+	 * get the count for a word. 
+	 * If the word doesn't occur in the tree, 0 is returned.
+	 */
+	public int getCount(String word) {
+		return root.getCount(word);
+	}
+	
+	/*
+	 * The method prints nodes whose word and count satisfy the 
+	 * condition in the selector, i.e. for which selector.test(word, count)
+	 * returns true. 
+	 * The method starts the recursive process by calling the inOrderRec 
+	 * and passing the root to it. 
+	 */
+	public void inOrder(BiPredicate<String,Integer> selector) {
+		inOrderRec(root, selector);
+	}
+	
+	/*
+	 * A recursive in-order tree traversal that prints elements that satisfy
+	 * the predicate in selector
+	 */
+	private void inOrderRec(BSTNode node, BiPredicate<String,Integer> selector) {
+		// Fill in the code according to the description
+		// Hint: use selector.test(...) to call the selector function. 
+		// It returns a boolean (according to the BiPredicate interface) 
+		// which determines whether the data in the node should be printed or not. 
+	}
+	
+
+}
---
+++ src/bst/TestBST.java
@@ -0,0 +1,48 @@
+package bst;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class TestBST {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		BSTCount wordCounts = new BSTCount();
+		
+		/*
+		 * Reading data from a file word-by-word by specifying spaces as a delimiter
+		 */
+		Scanner sc = new Scanner(new File("data.txt")).useDelimiter("\\s");
+		while (sc.hasNext()) {
+			String s = sc.next();
+			// removing all non-letter characters by replacing them with ""
+			s = s.replaceAll("[^a-zA-Z]", "");
+			// if the resulting string is non-empty, add it to the BST
+			// You may start by trying out just printing it
+
+		}
+		
+//		System.out.println("Winnie: " + wordCounts.getCount("Winnie"));
+//		System.out.println("Winnie-the-Pooh: " + wordCounts.getCount("Winnie-the-Pooh"));
+//		System.out.println("WinniethePooh: " + wordCounts.getCount("WinniethePooh"));
+		
+		//wordCounts.inOrder((s,n)-> true); // print all words and counts
+		//wordCounts.inOrder((s,n)-> (n >= 5)); // print only nodes with counts >= 5
+		// Add: print nodes with the words longer than 5 letters
+		// Add another call to inOrder using "and", "or", or "negate"
+		
+		
+		// Add a method "compute" to BSTCount. The method also performs an in-order traversal
+		// recursively. 
+		// It takes a lambda function that takes two parameters (a string and an integer)
+		// and returns an int.
+		// The function implements the following interface:
+		// https://docs.oracle.com/en/java/javase/20/docs/api/java.base/java/util/function/ToIntBiFunction.html
+		
+		// After you implement the traversal function, use it to:
+		// 1. Find the sum of all counts of words. 
+		// 2. Find the sum of the lengths of all words.
+		
+	}
+
+}
